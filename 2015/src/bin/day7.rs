1d81c094c4779199be7bebe8e5086b6c76a91a72
Martin Lemesle <martin.lemesle@iomentum.com>
2022-12-28 13:41:50 +0100

    day 7 2015

--- 2015/src/bin/day7.rs
+++ 2015/src/bin/day7.rs
@@ -18,11 +18,20 @@ impl FromStr for Value {
     }
 }
 
+impl Value {
+    fn resolve(&self, wires: &mut HashMap<String, Data>) -> u16 {
+        match self {
+            Value::Number(n) => *n,
+            Value::Wire(w) => resolve(w, wires),
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 enum Operation {
     Assign(Value),
-    And(String, String),
-    Or(String, String),
+    And(Value, Value),
+    Or(Value, Value),
     RShift(String, u16),
     LShift(String, u16),
     Not(String),
@@ -47,11 +56,11 @@ impl FromStr for Instruction {
             3 => match op_parts[1] {
                 "AND" => Self(
                     target.into(),
-                    Operation::And(op_parts[0].into(), op_parts[2].into()),
+                    Operation::And(op_parts[0].parse()?, op_parts[2].parse()?),
                 ),
                 "OR" => Self(
                     target.into(),
-                    Operation::Or(op_parts[0].into(), op_parts[2].into()),
+                    Operation::Or(op_parts[0].parse()?, op_parts[2].parse()?),
                 ),
                 "RSHIFT" => Self(
                     target.into(),
@@ -70,19 +79,59 @@ impl FromStr for Instruction {
     }
 }
 
-fn part1(instructions: &mut HashMap<String, Operation>) {
-    let a = wires.get("a").unwrap_or(&0);
+#[derive(Debug, Clone)]
+enum Data {
+    Resolved(u16),
+    ToResolve(Operation),
+}
 
-    println!("Final signal on wire a is {a}");
+fn resolve(wire: &str, wires: &mut HashMap<String, Data>) -> u16 {
+    let entry = wires.get(wire).cloned();
+    match entry {
+        Some(data) => match data {
+            Data::Resolved(signal) => signal,
+            Data::ToResolve(operation) => {
+                let signal = match operation {
+                    Operation::Assign(value) => value.resolve(wires),
+                    Operation::And(value1, value2) => value1.resolve(wires) & value2.resolve(wires),
+                    Operation::Or(value1, value2) => value1.resolve(wires) | value2.resolve(wires),
+                    Operation::RShift(w, shift) => resolve(&w, wires) >> shift,
+                    Operation::LShift(w, shift) => resolve(&w, wires) << shift,
+                    Operation::Not(w) => !resolve(&w, wires),
+                };
+                wires.insert(wire.to_string(), Data::Resolved(signal));
+                signal
+            }
+        },
+        None => {
+            unreachable!("there is a hole in the `wires` HashMap, wire {wire} does not exist ?")
+        }
+    }
+}
+
+fn part1(instructions: &mut HashMap<String, Data>) {
+    let a = resolve("a", instructions);
+
+    println!("Final signal on wire a is {a:?}");
+}
+
+fn part2(instructions: &mut HashMap<String, Data>) {
+    let a = resolve("a", &mut instructions.clone());
+    instructions.insert("b".to_string(), Data::Resolved(a));
+
+    let new_a = resolve("a", instructions);
+
+    println!("Super ultimate final signal on wire a is {new_a:?}");
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut wires = lib::input::<Instruction>("input/day7.txt")?
-        .iter()
-        .map(|instruction| (instruction.0, instruction.1))
-        .collect::<HashMap<String, Operation>>();
+    let mut instructions = lib::input::<Instruction>("input/day7.txt")?
+        .into_iter()
+        .map(|instruction| (instruction.0, Data::ToResolve(instruction.1)))
+        .collect::<HashMap<String, Data>>();
 
-    part1(&instructions);
+    part1(&mut instructions.clone());
+    part2(&mut instructions);
 
     Ok(())
 }
